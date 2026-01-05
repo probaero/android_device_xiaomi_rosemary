3ddeed6af3bb68df99d615bf32e3ef7b1c25a465
kenway214 <kenway214@outlook.com>
2026-01-06 01:03:13 +0530

    rosemary: parts: Partially import parts from sm8350-common
    Added:
    
    * Thermals
    * Per-app refresh rate
    * Clear speaker
    * Doze
    
    Change-Id: 6b40e6a2aaf53e3a7438bc44b840f16362jdb861
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Co-Authored-By: Bruno Martins <bgcngm@gmail.com>
    Co-Authored-By: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-Authored-By: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-Authored-By: nullxception <enuma.alrizky@gmail.com>
    Co-Authored-By: Sebastiano Barezzi <barezzisebastiano@gmail.com>
    Co-Authored-By: TheScarastic <warabhishek@gmail.com>

--- device.mk
+++ device.mk
@@ -93,6 +93,10 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Display
 TARGET_SCREEN_DENSITY := 440
 TARGET_SCREEN_HEIGHT := 2400
